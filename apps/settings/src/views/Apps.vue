ddd802bfa7e535441dc3014aa2ddd8ab3a913234
Christoph Wurst <christoph@winzerhof-wurst.at>
2019-11-26 18:51:10 +0100

    fixup! Add checkbox to install recommended apps during setup
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- apps/settings/src/views/Apps.vue
+++ apps/settings/src/views/Apps.vue
@@ -134,12 +134,20 @@ export default {
 					text: t('settings', 'Your apps')
 				},
 				{
+					id: 'app-category-recommended',
+					classes: [],
+					router: { name: 'apps-category', params: { category: 'recommended' } },
+					icon: 'icon-category-installed',
+					text: t('settings', 'Recommended apps')
+				},
+				{
 					id: 'app-category-enabled',
 					classes: [],
 					icon: 'icon-category-enabled',
 					router: { name: 'apps-category', params: { category: 'enabled' } },
 					text: t('settings', 'Active apps')
-				}, {
+				},
+				{
 					id: 'app-category-disabled',
 					classes: [],
 					icon: 'icon-category-disabled',
