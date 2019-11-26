327619bbccda3b466dfa1d4587f19723b09e0fa8
Maxence Lange <maxence@artificial-owl.com>
2019-11-26 18:45:10 -0100

    extends LockedException
    Signed-off-by: Maxence Lange <maxence@artificial-owl.com>

---
+++ lib/public/Lock/TemporaryLockedException.php
@@ -0,0 +1,66 @@
+<?php
+declare(strict_types=1);
+/**
+ * @copyright Copyright (c) 2016, ownCloud, Inc.
+ *
+ * @author Maxence Lange <maxence@artificial-owl.com>
+ *
+ * @license AGPL-3.0
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+namespace OCP\Lock;
+
+/**
+ * Class LockedException
+ *
+ * @package OCP\Lock
+ * @since 18.0.0
+ */
+class TemporaryLockedException extends LockedException {
+
+	/**
+	 * estimated timeout for the lock
+	 *
+	 * @var int
+	 * @since 18.0.0
+	 */
+	private $timeout = -1;
+
+	/**
+	 * TemporaryLockedException constructor.
+	 *
+	 * @param string $path locked path
+	 * @param \Exception|null $previous previous exception for cascading
+	 * @param string $existingLock
+	 * @param int $timeout
+	 *
+	 * @since 18.0.0
+	 */
+	public function __construct(string $path, \Exception $previous = null, string $existingLock = null, int $timeout = -1) {
+		parent::__construct($path, $previous, $existingLock);
+		$this->timeout = $timeout;
+	}
+
+
+	/**
+	 * @return int
+	 * @since 18.0.0
+	 */
+	public function getTimeout(): int {
+		return $this->timeout;
+	}
+
+}
